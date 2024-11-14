325412483aeeee02c5caef6b081bfbf3917e2883
Tomas Calderaro <tomasjcalderaro@gmail.com>
2024-11-14 14:43:42 -0300

    Primer Componente React

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ReactDOM } from 'react-dom/client'
+import {PrimerComponente} from './PrimerComponente'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PrimerComponente />
   </StrictMode>,
 )
